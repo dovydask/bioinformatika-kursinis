3d7bc019e09706179eae0e320724c96d8ce085b0
dovydask <d.kiciatovas@gmail.com>
2017-04-20 18:47:26 +0300

    Pridedu 2 neur. tinklus
    3-jų ir 4-ių sluoksnių tinklai su mini-batch gradientu (galima naudoti ir stochastinį).

---
+++ 4_layer_network.py
@@ -0,0 +1,155 @@
+import numpy as np
+np.set_printoptions(threshold=np.nan)
+
+# Paprasto 4-ių sluoksnių sigmoid neuronų tinklo simuliacija. 
+# 
+# Tinklas treniruotei gauna kokį nors skaičių atsitiktinių 
+# 8-ių bitų masyvų su atitinkamais taikiniais (9 elementų masyvais). 
+# 
+# Tinklo testavimui pateikiamas nurodytas skaičius atsitiktinių įvesties 
+# masyvų (taip pat 8 elementų). 
+# 
+# Naudojamas "Mini-batch" gradientas (nustačius mini_batch_size = 1 
+# gaunamas stochastinis gradientas).
+# 
+# Tinklas pagal gautą treniruotę bando nustatyti, kiek vienetų yra įvestyje.
+#
+# Autorius: Dovydas Kičiatovas
+
+# np.random.seed(1)
+
+# Tinklo nustatymai
+example_count = 200		# Treniruotei skirtų pavyzdžių skaičius
+input_count = 50		# Testavimui skirtų įvesčių skaičius
+mini_batch_size = 15	# Pavyzdžių poaibio dydis mini-batch gradientui
+epochs = 2000			# Treniruočių (iteracijų) skaičius
+bit_count = 8			# Pavyzdinio masyvo dydis (bitų skaičius)
+learning_rate = 1		# Mokymosi greitis (miu)1
+
+#Sigmoid funkcija
+def sigmoid(x, derivative=False):	
+	if(derivative==True):
+		return x*(1-x)
+	return 1/(1+np.exp(-x))
+
+#Treniruotės masyvas (atsitiktinis)
+examples = np.zeros((example_count, bit_count), dtype=np.int)	
+for i in range(examples.shape[0]):
+	for j in range(examples.shape[1]):
+		examples[i][j] = np.random.randint(0, 2)
+		
+#Įvesties masyvas (atsitiktinis)						
+input = np.zeros((input_count, bit_count))
+for i in range(input.shape[0]):
+	for j in range(input.shape[1]):
+		input[i][j] = np.random.randint(0, 2)
+
+#Tikrasis įvestyse esančių bitų skaičius (rezultato palyginimui)
+input_value = np.zeros(input.shape[0])
+for i in range(input.shape[0]):
+	counter = 0
+	for j in range(input.shape[1]):
+		if(input[i][j] == 1):
+			counter = counter + 1
+	input_value[i] = counter
+
+#Tikroji pavyzdžių reikšmė (pageidaujama išvestis - taikiniai)
+output = np.zeros((examples.shape[0], examples.shape[1]+1), dtype=np.int)
+for i in range(examples.shape[0]):
+	counter = 0
+	for j in range(examples.shape[1]):
+		if(examples[i][j] == 1):
+			counter = counter + 1
+	output[i][counter] = 1
+
+#Sinapsės inicializuojamos su atsitiktiniais svoriais
+synapse_1 = 2*np.random.random((bit_count, mini_batch_size)) - 1
+synapse_2 = 2*np.random.random((mini_batch_size, mini_batch_size)) - 1
+synapse_3 = 2*np.random.random((mini_batch_size, bit_count+1)) - 1
+
+#Pavyzdžių poaibių sudarymas
+def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+    assert inputs.shape[0] == targets.shape[0]
+    if shuffle:
+        indices = np.arange(inputs.shape[0])
+        np.random.shuffle(indices)
+    for start_idx in range(0, inputs.shape[0] - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs[excerpt], targets[excerpt]
+	
+#Tinklo treniravimas
+for x in range(epochs):
+	for batch in iterate_minibatches(examples, output, mini_batch_size, False):
+		
+		batch_input, batch_output = batch
+		
+		layer_0 = batch_input
+		output_ref = batch_output
+				
+		layer_1 = sigmoid(np.dot(layer_0, synapse_1))
+		layer_2 = sigmoid(np.dot(layer_1, synapse_2))
+		layer_3 = sigmoid(np.dot(layer_2, synapse_3))
+				
+		layer_3_error = layer_3 - output_ref
+		layer_3_delta = layer_3_error*sigmoid(layer_3, True)
+				
+		layer_2_error = layer_3_delta.dot(synapse_3.T)
+		layer_2_delta = layer_2_error*sigmoid(layer_2, True)
+				
+		layer_1_error = layer_2_delta.dot(synapse_2.T)
+		layer_1_delta = layer_1_error*sigmoid(layer_1, True)
+		
+		layer_1 = np.matrix(layer_1)
+		layer_2_delta = np.matrix(layer_2_delta)
+		layer_2 = np.matrix(layer_2)
+		layer_3 = np.matrix(layer_3)
+		layer_3_delta = np.matrix(layer_3_delta)
+				
+		synapse_3 -= learning_rate * layer_2.T.dot(layer_3_delta)
+		synapse_2 -= learning_rate * layer_1.T.dot(layer_2_delta)
+		synapse_1 -= learning_rate * layer_0.T.dot(layer_1_delta)
+			
+#Tinklo testavimas
+print("Spėjimas / Tikroji reikšmė")
+correct_guesses = 0		#Teisingų spėjimų skaičius
+ind_taiklumas = []		#Kiekvienos įvesties bitų skaičiaus spėjimo tikslumas
+for i in range(input.shape[0]):
+	layer_0 = input[i]
+	
+	layer_1 = sigmoid(np.dot(layer_0, synapse_1))
+	layer_2 = sigmoid(np.dot(layer_1, synapse_2))
+	layer_3 = sigmoid(np.dot(layer_2, synapse_3))
+	
+	temp = None
+	index1 = 0
+	index2 = input_value[i]
+	for j in range(input.shape[1]):
+		if(temp is None or layer_3[j] > temp):
+			temp = layer_3[j]
+			index1 = j
+		
+	print(index1, " / ", int(index2))
+	if(index1 == index2):
+		if(index2 == 0):
+			index2 = 1
+		correct_guesses = correct_guesses + 1
+		ind_taiklumas.append(index1/index2)
+	elif(index1 > index2):
+		if(index1 == 0):
+			index1 = 1
+		ind_taiklumas.append(index2/index1)
+	elif(index1 < index2):
+		if(index2 == 0):
+			index2 = 1
+		ind_taiklumas.append(index1/index2)
+		
+print("Teisingi spėjimai: ", correct_guesses, " iš ", input.shape[0], "(", correct_guesses/input.shape[0], ")")
+
+sum = 0
+for i in range(len(ind_taiklumas)):
+	sum = sum + ind_taiklumas[i]
+	
+print("Individualių spėjimų taiklumo vidurkis: ", sum/len(ind_taiklumas))	
